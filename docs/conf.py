d79e1235bddcaa4911ecf80013e261cd9d3a0d1f
mqichu <mqichu@anl.gov>
2026-07-15 17:12:55 -0500

    docs: exclude session planning artifacts from Sphinx build

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ extensions = [
 ]
 
 templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'superpowers/**']
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
